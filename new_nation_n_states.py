a3f4db2281a9ec2a2addb0dee1fad660a7b10713
bhona005 <bhona005@umn.edu>
2023-07-26 09:58:50 -0500

    corrected logic

--- new_nation_n_states.py
+++ new_nation_n_states.py
@@ -14,7 +14,7 @@ def new_nation_n_states(number_len, usstates, border_data):
     us_states = pd.read_csv(usstates, header=None).rename(
         columns={0: "State", 1: "State Code", 2: "Area", 3: "Population"})
     us_states = us_states.drop_duplicates(subset=['State Code'], keep='first').sort_values(by=['Population'], ascending=False)
-    top_50_pct = us_states["State Code"][:int(len(us_states["State Code"])/2+1)].to_list()
+    #top_50_pct = us_states["State Code"][:int(len(us_states["State Code"])/2+1)].to_list()
     state_population = dict(zip(us_states['State Code'], us_states['Population']))
 
     border_data = pd.read_csv(border_data)
@@ -29,8 +29,8 @@ def new_nation_n_states(number_len, usstates, border_data):
     borders_defined_dict = borders_defined.groupby('State')['border state'].apply(list).to_dict()
 
     set_states = set()
-    #for state in borders_defined_dict.keys():
-    for state in top_50_pct:
+    for state in borders_defined_dict.keys():
+        #for state in top_50_pct:
         new_step = [[state]]
         number = 0
         while number < number_len - 1:
