0db4142bdf4da793ef1bbfdbd5eff04884d273df
bhona005 <bhona005@umn.edu>
2023-07-28 20:07:47 -0500

    Further optimized

--- new_nation_n_states.py
+++ new_nation_n_states.py
@@ -1,4 +1,5 @@
 import pandas as pd
+
 def get_max_pop_set(sets_of_states, state_population):
     max_set = []
     max_population = 0
@@ -9,48 +10,40 @@ def get_max_pop_set(sets_of_states, state_population):
             max_set = state_set
     return (max_set, max_population)
 
-
 def new_nation_n_states(number_len, usstates, border_data):
-    us_states = pd.read_csv(usstates, header=None).rename(
-        columns={0: "State", 1: "State Code", 2: "Area", 3: "Population"})
+    us_states = pd.read_csv(usstates, header=None, names=["State", "State Code", "Area", "Population"])
     us_states = us_states.drop_duplicates(subset=['State Code'], keep='first').sort_values(by=['Population'], ascending=False)
-    #top_50_pct = us_states["State Code"][:int(len(us_states["State Code"])/2+1)].to_list()
-    #top_50_pct = ["CO","IL","MO","AZ","TX","OK","CA"]
     state_population = dict(zip(us_states['State Code'], us_states['Population']))
 
     border_data = pd.read_csv(border_data)
     borders_defined = border_data["ST1ST2"].str.split("-", expand=True)
     borders_defined = borders_defined.rename(columns={0: "State", 1: "border state"})
-    borders_defined_rev = borders_defined.rename(columns={"State":"border state", "border state":"State"})
-    borders_defined = pd.concat([borders_defined, borders_defined_rev]).reset_index(drop = True)
-    borders_defined = borders_defined[borders_defined["State"] != borders_defined["border state"]].reset_index(
-        drop=True)
+    borders_defined_rev = borders_defined.rename(columns={"State": "border state", "border state": "State"})
+    borders_defined = pd.concat([borders_defined, borders_defined_rev]).reset_index(drop=True)
+    borders_defined = borders_defined[borders_defined["State"] != borders_defined["border state"]].reset_index(drop=True)
     borders_defined = borders_defined.drop_duplicates(subset=['State', 'border state'], keep='first')
 
-    borders_defined_dict = borders_defined.groupby('State')['border state'].apply(list).to_dict()
+    borders_defined_dict = borders_defined.groupby('State')['border state'].apply(set).to_dict()
 
     set_states = set()
     for state in borders_defined_dict.keys():
-        #for state in top_50_pct:
-        new_step = [[state]]
+        new_step = {frozenset([state])}
         number = 0
         while number < number_len - 1:
             prev_step = new_step.copy()
-            new_step = []
+            new_step = set()
             for step in prev_step:
-                if len(step)>1:
-                    states_neighbours = set(borders_defined_dict[step[-2]]+borders_defined_dict[step[-1]])
+                if len(step) > 1:
+                    states_neighbours = {neighbour for s in step for neighbour in borders_defined_dict.get(s, set())}
                 else:
-                    states_neighbours = borders_defined_dict[step[-1]]
+                    states_neighbours = borders_defined_dict.get(next(iter(step)), set())
                 for neighbour in states_neighbours:
-                    new_step.append(step + [neighbour])
-            number = number + 1
-        new_step = [frozenset(x) for x in new_step if len(set(x)) == number_len]
-        new_step = set(new_step)
-        set_states = set_states.union(new_step)
-    set_states_distinct = list(set_states)
+                    new_step.add(frozenset(step | {neighbour}))
+            number += 1
+        set_states |= new_step
+    #set_states_distinct = list(set_states)
 
-    return get_max_pop_set(set_states_distinct, state_population)
+    return get_max_pop_set(set_states, state_population)
 
-#result = new_nation_n_states(6, 'usstates.csv', 'border_data.csv')
+#result = new_nation_n_states(8, 'usstates.csv', 'border_data.csv')
 #print(result)
