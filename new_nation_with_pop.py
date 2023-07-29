27dee72d2230aca464cd65a030139193343f7758
bhona005 <bhona005@umn.edu>
2023-07-29 10:31:54 -0500

    Add files via upload

--- new_nation_with_pop.py
+++ new_nation_with_pop.py
@@ -52,5 +52,5 @@ def new_nation_with_pop(min_pop, usstates, border_data):
         number_len = number_len + 1
     return required_pop_sets
 
-result = new_nation_with_pop(85, 'usstates.csv', 'border_data.csv')
-print(result)
+#result = new_nation_with_pop(85, 'usstates.csv', 'border_data.csv')
+#print(result)
